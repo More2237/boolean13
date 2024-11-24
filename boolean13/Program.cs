3260550baf002373dc22dcbfa40ab81aeb6bc0dd
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-24 12:33:54 +0500

    nner

---
+++ boolean13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace boolean13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+            int b = int.Parse(ReadLine());
+            int c = int.Parse(ReadLine());
+            
+            bool result = a>0||b>0||c>0;
+            WriteLine(result);
+        }
+    }
+}
